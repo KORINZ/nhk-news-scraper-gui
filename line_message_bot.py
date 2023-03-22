ebce14e235ba0c58dfd957d345e3979817dc1a14
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-03-22 19:14:33 +0900

    Reformat code structure

--- line_message_bot.py
+++ line_message_bot.py
@@ -4,6 +4,10 @@ from linebot.exceptions import LineBotApiError
 from config import CHANNEL_ACCESS_TOKEN, USER_ID
 
 
+# CHANNEL_ACCESS_TOKEN = ''
+# USER_ID = ''
+
+
 def send_message(text) -> None:
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
@@ -14,7 +18,6 @@ def send_message(text) -> None:
 
 
 if __name__ == "__main__":
-
     with open('sample_test.txt', 'r', encoding='utf-8') as f:
         content = f.read()
 
