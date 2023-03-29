c0024e53c93e11c17e48d8d65cf5c67565fc334c
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-03-29 15:21:07 +0900

    Add LINE confidential user input

--- quiz_generator_gui.py
+++ quiz_generator_gui.py
@@ -4,10 +4,80 @@ from main import main, push_quiz
 import tkinter.ttk as ttk
 import threading
 
+import os
+from tkinter import filedialog
+
 VERSION = "v1.0.0"
 PRONOUN_QUIZ_LOCATION = r'./pronunciation_quiz.txt'
 DEF_QUIZ_LOCATION = r'./definition_quiz.txt'
 is_blinking = False
+
+
+def save_text_to_file() -> None:
+    """Save the content of the Text widget to a file."""
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                             filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if not file_path:
+        return
+
+    content = article_text.get("1.0", tk.END)
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(content)
+
+
+def start_over() -> None:
+    """Reset the app to its initial state."""
+    article_text.delete("1.0", tk.END)
+    questions_entry.delete(0, tk.END)
+    questions_entry.insert(0, "5")
+    test_type_combobox.current(1)
+    line_push_var.set(False)
+    load_article_button.config(state="disabled")
+    load_def_quiz_button.config(state="disabled")
+    load_pron_quiz_button.config(state="disabled")
+    load_log_button.config(state="disabled")
+    send_button.config(state="disabled")
+
+
+def enter_line_confidential() -> None:
+    """Display a popup to enter LINE confidential information."""
+    line_confidential_popup = tk.Toplevel(root)
+    line_confidential_popup.title("LINE機密情報入力")
+    line_confidential_popup.iconbitmap(r'icon/LINE.ico')
+
+    tk.Label(line_confidential_popup, text="CHANNEL_ACCESS_TOKEN:").grid(
+        row=0, column=0, sticky="w")
+    channel_access_token_entry = tk.Entry(line_confidential_popup)
+    channel_access_token_entry.grid(
+        row=0, column=1, padx=5, pady=5, sticky="w")
+
+    tk.Label(line_confidential_popup, text="USER_ID:").grid(
+        row=1, column=0, sticky="w")
+    user_id_entry = tk.Entry(line_confidential_popup)
+    user_id_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+
+    save_button = tk.Button(line_confidential_popup, text="保存", command=lambda: save_line_confidential(
+        channel_access_token_entry.get(), user_id_entry.get()))
+    save_button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
+
+    # Add a "Cancel" button
+    cancel_button = tk.Button(
+        line_confidential_popup, text="キャンセル", command=line_confidential_popup.destroy)
+    cancel_button.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+
+
+def save_line_confidential(channel_access_token: str, user_id: str) -> None:
+    """Save the LINE confidential information to a config.py file."""
+    with open("config.py", "w", encoding="utf-8") as config_file:
+        config_file.write(f"CHANNEL_ACCESS_TOKEN = '{channel_access_token}'\n")
+        config_file.write(f"USER_ID = '{user_id}'\n")
+    messagebox.showinfo("成功", "LINE機密情報が保存されました！")
+
+
+def show_credit_popup() -> None:
+    """Display a popup with the version number and credit information."""
+    credit_info = "バージョン: " + VERSION + "\n開発者: KORIN\n年: 2023"
+    messagebox.showinfo("クレジット", credit_info)
 
 
 def start_quiz_generation_thread() -> None:
@@ -37,7 +107,7 @@ def run_quiz_generation() -> None:
         load_log_button.config(state="normal")
         is_blinking = False
         status_label.config(text="")
-    except Exception:
+    except ValueError:
         messagebox.showerror("エラー", "問題数を指定してください。")
         is_blinking = False
         status_label.config(text="")
@@ -98,6 +168,37 @@ status_label.grid(row=3, column=1, padx=(230, 0), sticky="w")
 test_type_var = tk.StringVar()
 line_push_var = tk.BooleanVar()
 questions_var = tk.StringVar()
+
+
+# Create a menu bar
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+# Create a "File" menu
+file_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="ファイル", menu=file_menu)
+
+# Add a "Save As" option to the "File" menu
+file_menu.add_command(label="表示しているファイル名前を付けて保存", command=save_text_to_file)
+
+# Create a "Tool" menu
+tool_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="ツール", menu=tool_menu)
+
+# Add a "Start Over" option to the "Tool" menu
+tool_menu.add_command(label="やり直し", command=start_over)
+
+# Add an "Enter LINE Confidential" option to the "Tool" menu
+tool_menu.add_command(label="LINE機密情報入力", command=enter_line_confidential)
+
+# Create an "About" menu
+about_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="情報", menu=about_menu)
+
+# Add a "Credit" option to the "About" menu
+about_menu.add_command(
+    label=f"NHK NEWS EASY クイズ作成 GUI {VERSION}について", command=show_credit_popup)
+
 
 # Set the default value for test_type_var
 test_type_var.set("単語意味クイズ")
