acf702895f9223df74bf328434f18b22e716edb0
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-04-19 02:56:35 +0900

    Add playground for OCR

---
+++ playground.py
@@ -0,0 +1,93 @@
+"""A playground for testing out OCR on images of Japanese text."""
+
+# Standard library imports
+import logging
+import os
+from typing import Tuple, Optional
+
+# Third-party imports
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+# Set the logging level
+logging.basicConfig(level=logging.INFO)
+
+# Set the path to the Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+
+def preprocess_image(
+    image: Image.Image,
+    size: Optional[Tuple[int, int]] = None,
+    contrast: float = 2.0,
+    sharpness: float = 2.0,
+    denoise_kernel_size: int = 3,
+    threshold: int = 135,
+) -> Image.Image:
+    """Preprocess an image to improve OCR accuracy."""
+    if size is not None:
+        image = image.resize(size, Image.BICUBIC)
+
+    # Convert image to grayscale
+    image = image.convert("L")
+
+    # Enhance contrast
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(contrast)
+
+    # Enhance sharpness
+    enhancer = ImageEnhance.Sharpness(image)
+    image = enhancer.enhance(sharpness)
+
+    # Apply a median filter (denoising)
+    image = image.filter(ImageFilter.MedianFilter(denoise_kernel_size))
+
+    # Apply adaptive thresholding
+    image = image.point(lambda x: 0 if x < threshold else 255, "1")
+
+    return image
+
+
+def ocr_image(
+    image_path: str,
+    lang: str = "jpn",
+    preprocess: bool = True,
+    preprocess_params: Optional[dict] = None,
+) -> str:
+    """Perform OCR on an Japanese text image and return the result."""
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f"File '{image_path}' does not exist.")
+
+    image = Image.open(image_path)
+
+    if preprocess:
+        if preprocess_params is not None:
+            logging.info("Preprocessing image...\n")
+            image = preprocess_image(image, **preprocess_params)
+        else:
+            image = preprocess_image(image)
+
+        # Display the preprocessed image
+        image.show()
+
+    # psm 6: Assume a single uniform block of text. oem 3: Use LSTM neural network.
+    config = "--psm 6 --oem 3 -c preserve_interword_spaces=1"
+    text = pytesseract.image_to_string(image, lang=lang, config=config)
+    return text.strip().replace(" ", "")
+
+
+if __name__ == "__main__":
+    """
+    The main script that performs OCR on a specified image and prints the result.
+    """
+    image_path = r"./ocr_images/2.png"
+    try:
+        with Image.open(image_path) as image:
+            result = ocr_image(
+                image_path,
+                preprocess_params={
+                    "size": (image.width * 4, image.height * 4)},
+            )
+            print(result)
+    except FileNotFoundError:
+        print(f"File '{image_path}' does not exist.")
